3d6b5366827c782ef9be2c162713a4d00b1af050
joan-gilabert-vg <Joan-Gilabert@users.noreply.gitlab.com>
2022-04-01 11:28:11 +0200

    Nueva rama para laboratio conductor con workflow core.

---
+++ docs/convert.bat
@@ -0,0 +1,11 @@
+  E:\GitHub\WorkFlows\Workflow-Core\conductor\docs
+
+pandoc -f markdown -t docx api-reference.md 				-o api-reference.docx 
+pandoc -f markdown -t docx auth.md                          -o auth.docx
+pandoc -f markdown -t docx custom-steps.md                  -o custom-steps.docx
+pandoc -f markdown -t docx geting-started.md                -o geting-started.docx
+pandoc -f markdown -t docx http-walkthru.md                 -o http-walkthru.docx
+pandoc -f markdown -t docx index.md                         -o index.docx
+pandoc -f markdown -t docx installation.md                  -o installation.docx
+pandoc -f markdown -t docx primitives.md                    -o primitives.docx
+pandoc -f markdown -t docx roadmap.md                       -o roadmap.docx
